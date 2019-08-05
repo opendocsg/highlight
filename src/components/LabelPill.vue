b61f3af993325a95e5e15da64c98896970d5b8e0
Donald Lee <donaldleeqs@gmail.com>
2019-08-05 23:34:27 +0800

    add label pills

--- src/components/LabelPill.vue
+++ src/components/LabelPill.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="label-pill">
-    {{ label.type }}
+    <button 
+      type="button" 
+      class="btn btn-lg" 
+      :style="{'background-color': label.color, color:'white'}"
+    >
+      {{ label.type }} 
+      <span class="badge badge-light">{{ label.annotations.length }}</span>
+    </button>
   </div>
 </template>
 
